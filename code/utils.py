b8a93dde45fe84aeb902db3ed27371b45f116766
ArbaazBeg15 <31arbazk31@gmail.com>
2025-08-21 12:11:02 +0000

    update

--- code/utils.py
+++ code/utils.py
@@ -1,8 +1,10 @@
 import random
 import torch
 import numpy as np
+from scipy import signal
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 
 def setup_reproducibility():
@@ -68,3 +70,26 @@ def show_waves(waves, dpi=100):
         ax.grid(True)
         fig.tight_layout()  # reduce margins to use width
     plt.show()
+    
+    
+def hf_ds_download(token, repo_id):
+    login(hf_token)
+    return snapshot_download(repo_id, repo_type="dataset")
+
+
+def get_advanced_spectra_features(X):
+    """Create multi-channel features from spectra: raw, 1st derivative, 2nd derivative."""
+    X_processed = np.zeros_like(X)
+    # Baseline correction and SNV
+    for i in tqdm(range(X.shape[0])):
+        poly = np.polyfit(np.arange(X.shape[1]), X[i], 3)
+        baseline = np.polyval(poly, np.arange(X.shape[1]))
+        corrected_spec = X[i] - baseline
+        X_processed[i] = (corrected_spec - corrected_spec.mean()) / (corrected_spec.std() + 1e-8)
+
+    # Calculate derivatives
+    deriv1 = signal.savgol_filter(X_processed, window_length=11, polyorder=3, deriv=1, axis=1)
+    deriv2 = signal.savgol_filter(X_processed, window_length=11, polyorder=3, deriv=2, axis=1)
+
+    # Stack as channels
+    return np.stack([X_processed, deriv1, deriv2], axis=1)
---
+++ code/xgb.py
@@ -0,0 +1,76 @@
+import os
+import pandas as pd
+import optuna
+from xgboost import XGBRegressor
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+from utils import *
+
+
+SEED = 1000
+HF_TOKEN = "xhf_uOkImkbEroqtIuyvGJrttTzaebfeIdPZID"
+
+
+def main():
+    setup_reproducibility(SEED)
+    path = hf_ds_download(HF_TOKEN, repo_id="ArbaazBeg/kaggle-spectogram")
+    files = sorted(os.listdir(path))
+    
+    csv_path = os.path.join(path, files[11])
+    df = pd.read_csv(csv_path)
+
+    input_cols = df.columns[1:2049]
+    target_cols = df.columns[2050:]
+
+    targets  = df[target_cols].dropna().to_numpy()
+
+    df = df[input_cols]
+    df['Unnamed: 1'] = df['Unnamed: 1'].str.replace("[\[\]]", "", regex=True).astype('int64')
+    df['Unnamed: 2048'] = df['Unnamed: 2048'].str.replace("[\[\]]", "", regex=True).astype('int64')
+
+    inputs = df.to_numpy().reshape(-1, 2, 2048)
+    inputs = inputs.mean(axis=1)
+    inputs = get_advanced_spectra_features(inputs)
+    
+    inputs = inputs.reshape(-1, 3 * 2048).astype(np.float32)
+    targets = targets.astype(np.float32)
+    
+    train_inputs, eval_inputs, train_targets, eval_targets = split(inputs, targets, SEED)
+    
+    _, _, mean, std = get_stats(train_inputs, p=False, r=True)
+    train_inputs = zscore(train_inputs, mean, std)
+    eval_inputs = zscore(eval_inputs, mean, std)
+
+    def objective(trial):
+        n_estimators = trial.suggest_int("n_estimators", 200, 1000)
+        max_depth = trial.suggest_int("max_depth", 3, 20)
+        learning_rate = trial.suggest_float("learning_rate", 0.0001, 0.8, log=True)
+        
+        model = XGBRegressor(
+            n_estimators=n_estimators,
+            learning_rate=learning_rate,
+            max_depth=max_depth,
+            subsample=0.8,
+            colsample_bytree=0.8,
+            random_state=SEED,
+            n_jobs=-1,
+            #reg_lambda=1.0,
+            eval_metric="rmse",  
+            #early_stopping_rounds=5,        
+            tree_method="hist", 
+            device="cuda",
+        )
+
+        model.fit(
+            train_inputs, train_targets,
+            eval_set=[(eval_inputs, eval_targets)],
+            verbose=False,
+        )
+
+        preds = model.predict(eval_inputs)
+        return r2_score(eval_targets, preds)
+
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective, n_trials=4, n_jobs=1)
+
+main()
